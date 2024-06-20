db1cb247d41dce6cd86b832e25d3f026f8ae28ac
PauloVitorAzzariti <paulovitorazzariti@gmail.com>
2024-06-20 04:00:11 -0300

    Site_Ingressos_Oficial.com.br

---
+++ script.js
@@ -0,0 +1,7 @@
+<script src="script.js"></script>
+function toggleviden() {
+    const trailer = document.querySelector('.trailer');
+    const video = document.querySelector('video');
+    video.pause();
+    trailer.classtist.togglel('active');
+}
